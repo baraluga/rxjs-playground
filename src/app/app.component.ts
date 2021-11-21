d1ffcbae3dde52525faa6237f8cedd41254002f3
Brian Alexander <brian.peralta@engie.com>
2021-11-21 16:07:08 +0800

    Added more operators: - Mergemap - Switchmap - Delay - DistinctUntilChanged - Pluck - Tap - Take - CatchError - Finalize - WithLatestFrom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,48 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Observable, OperatorFunction, Subject } from 'rxjs';
-import { debounceTime, filter, map } from 'rxjs/operators';
+import {
+  Observable,
+  of,
+  OperatorFunction,
+  pipe,
+  Subject,
+  throwError,
+} from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  delay,
+  distinctUntilChanged,
+  filter,
+  finalize,
+  map,
+  mergeMap,
+  pluck,
+  switchMap,
+  take,
+  tap,
+  withLatestFrom,
+} from 'rxjs/operators';
 
 enum RxJSOperator {
-  MAP = 'map',
-  FILTER = 'filter',
-  DEBOUNCE_TIME = 'debounceTime',
+  MAP = 'map(fn)',
+  FILTER = 'filter(fn)',
+  MERGE_MAP = 'mergeMap(fn)',
+  SWITCH_MAP = 'switchMap(fn)',
+  DELAY = 'delay(number)',
+  DEBOUNCE_TIME = 'debounceTime(number)',
+  DISTINCT_UNTIL_CHANGED = 'disctinctUntilChanged()',
+  PLUCK = 'pluck(keyOfObject)',
+  TAP = 'tap(fn)',
+  TAKE = 'take(number)',
+  CATCH_ERROR = 'catchError(fn)',
+  FINALIZE = 'finalize(fn)',
+  WITH_LATEST_FROM = 'withLatesFrom(observable$)',
 }
 
-type ValidOperatorFn = OperatorFunction<number | undefined, number | undefined>;
+type ValidOperatorFn = OperatorFunction<
+  number | undefined,
+  number | unknown[] | undefined
+>;
 
 @Component({
   selector: 'app-root',
@@ -17,7 +51,7 @@ type ValidOperatorFn = OperatorFunction<number | undefined, number | undefined>;
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  private before = new Subject<number>();
+  private nextSubject = new Subject<number>();
 
   selectedOperator: RxJSOperator = this.operators[0];
   nextValue = 1;
@@ -28,9 +62,33 @@ export class AppComponent {
 
   get operatorImpl(): Record<RxJSOperator, ValidOperatorFn> {
     return {
-      [RxJSOperator.MAP]: map((num) => num! * 1.23),
+      [RxJSOperator.MAP]: map((num) => num! * 2),
       [RxJSOperator.FILTER]: filter((num) => num! < 0),
       [RxJSOperator.DEBOUNCE_TIME]: debounceTime(1000),
+      [RxJSOperator.TAP]: tap(() =>
+        console.log('do something not affecting the stream!')
+      ),
+      [RxJSOperator.TAKE]: take(3),
+      [RxJSOperator.MERGE_MAP]: mergeMap((num) =>
+        of(num! * 3).pipe(delay(1000))
+      ),
+      [RxJSOperator.DELAY]: delay(2000),
+      [RxJSOperator.FINALIZE]: finalize(() =>
+        console.log('think "finally" in try/catches!')
+      ),
+      [RxJSOperator.PLUCK]: pipe(
+        map(() => ({ name: 'Brian', age: 29 })),
+        pluck('age')
+      ),
+      [RxJSOperator.WITH_LATEST_FROM]: withLatestFrom(of('BRLG')),
+      [RxJSOperator.SWITCH_MAP]: switchMap((num) =>
+        of(num! * 3).pipe(delay(1000))
+      ),
+      [RxJSOperator.DISTINCT_UNTIL_CHANGED]: distinctUntilChanged(),
+      [RxJSOperator.CATCH_ERROR]: pipe(
+        mergeMap(() => throwError('simulated error!')),
+        catchError((error) => of(error))
+      ),
     };
   }
 
@@ -39,7 +97,11 @@ export class AppComponent {
   }
 
   onNext(): void {
-    this.before.next(this.nextValue);
+    this.nextSubject.next(this.nextValue);
+  }
+
+  onComplete(): void {
+    this.nextSubject.complete();
   }
 
   private subscribeToAllImplOperators(): void {
@@ -60,12 +122,14 @@ export class AppComponent {
   private filterOnlyMatchingOperator(
     operator: RxJSOperator
   ): Observable<number> {
-    return this.before.pipe(filter(() => this.selectedOperator === operator));
+    return this.nextSubject.pipe(
+      filter(() => this.selectedOperator === operator)
+    );
   }
 
-  private log(value: number): void {
+  private log(value: unknown): void {
     console.log(
-      `-- ${this.selectedOperator.toUpperCase()} Operator --\nBEFORE: ${
+      `-- ${this.selectedOperator} Operator --\nBEFORE: ${
         this.nextValue
       }\nAFTER: ${value}\ntimestamp: ${new Date()}`
     );
