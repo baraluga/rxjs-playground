e91f2dd898c7efeeb9011fdbd2d51bdbe2903e34
Brian Alexander <brian.peralta@engie.com>
2021-11-21 15:25:38 +0800

    Map and Filter operators;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+enum RxJSOperator {
+  MAP = 'map',
+  FILTER = 'filter',
+}
 
 @Component({
   selector: 'app-root',
@@ -6,4 +13,55 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  private before = new Subject<number>();
+  private after = new Subject<number>();
+
+  selectedOperator: RxJSOperator = this.operators[0];
+  nextValue = 1;
+
+  get operators(): RxJSOperator[] {
+    return Object.values(RxJSOperator);
+  }
+
+  constructor() {
+    this.subscribeToAfterForLogging();
+
+    this.subscribeForMapOperator();
+    this.subscribeForFilterOperator();
+  }
+
+  onNext(): void {
+    this.before.next(this.nextValue);
+  }
+
+  private subscribeToAfterForLogging(): void {
+    this.after.subscribe((after) => this.log(after));
+  }
+
+  private subscribeForFilterOperator(): void {
+    this.filterOnlyMatchingOperator(RxJSOperator.FILTER)
+      .pipe(filter((num) => num > 1))
+      .subscribe((num) => this.after.next(num));
+  }
+
+  private subscribeForMapOperator(): void {
+    this.filterOnlyMatchingOperator(RxJSOperator.MAP)
+      .pipe(map((num: number) => num * 5))
+      .subscribe((num) => this.after.next(num));
+  }
+
+  private log(value: number): void {
+    console.log(
+      `-- ${this.selectedOperator.toUpperCase()} Operator --\nBEFORE: ${
+        this.nextValue
+      }\nAFTER: ${value}\ntimestamp: ${new Date()}`
+    );
+  }
+
+  private filterOnlyMatchingOperator(
+    operator: RxJSOperator
+  ): Observable<number> {
+    return this.before.pipe(filter(() => this.selectedOperator === operator));
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    FormsModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
